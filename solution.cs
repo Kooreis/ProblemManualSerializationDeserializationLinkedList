06f44e6bf8393c16df01df579a8f1a066a292baa
Kooreis <kooreis.official@gmail.com>
2024-10-24 03:33:52 -0500

    This is the LinkedList class definition. It represents the linked list itself. It has a head property and an append method. The append method is used to add a new node to the end of the list.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,18 @@
-class Node {
-    constructor(val, next = null) {
-        this.val = val;
-        this.next = next;
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(val) {
+        if (!this.head) {
+            this.head = new Node(val);
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = new Node(val);
     }
 }
